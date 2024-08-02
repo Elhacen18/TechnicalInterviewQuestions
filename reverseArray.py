4b7f124b4e7f5b2378ff400898121f6c63562ae7
Elhacen Elmoustapha <56775091+Elhacen18@users.noreply.github.com>
2024-08-01 20:15:17 -0400

    Update reverseArray.py

--- reverseArray.py
+++ reverseArray.py
@@ -1,4 +1,6 @@
 
+
+# Problem solving
 '''
 During my internship interview, I was asked a whiteboard question to reverse an array by swapping the first element with the last, the second with the second last, and so on. They wanted to see my thinking process and how I handled edge cases like an empty array or an array with one element.
 
@@ -21,5 +23,9 @@ def ReverseArray(Arr):
 ReverseArray([1,5,3,4,5])
 ReverseArray([3,2,7,4,5,6,7,8])
 
+'''
+outputs
+[5, 4, 3, 5, 1]
+[8, 7, 6, 5, 4, 7, 2, 3]
 
-    
+'''
